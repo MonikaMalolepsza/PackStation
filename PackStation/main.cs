ca8c07a58d23e8888024e2d2aa8d79ec6f94f23c
Monika <mm@froso.de>
2020-09-27 21:02:16 +0200

    initial commit

---
+++ PackStation/main.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PackStation
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
